4607444074e6233c46c605f4dbfa397972a54485
Ravi Ranjan Kumar DUBEY <38419795+ravi0dubey@users.noreply.github.com>
2024-07-03 19:08:09 -0400

    terraform and github actions (#2)
    * terraform and github actions
    
    * changes done to github actions

---
+++ terraform/main.tf
@@ -0,0 +1,98 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+  
+  backend "gcs" {
+    bucket  = "terraform-state-eh-ml-mentorship-4"
+    prefix  = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "racoon-detection-427421"  # Replace with your actual project ID
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# VPC Network
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = false
+
+  lifecycle {
+    ignore_changes = [name]  # Ignore changes to the network name
+  }
+}
+
+# Storage Buckets (applying the lifecycle pattern)
+resource "google_storage_bucket" "raw_dataset" {
+  name          = "01-raw_dataset-${random_string.bucket_suffix.result}"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket" "extracted_images" {
+  name          = "02-extracted-images-${random_string.bucket_suffix.result}"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket" "dedup_images" {
+  name          = "03-dedup-images-${random_string.bucket_suffix.result}"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket" "playground_bucket" {
+  name          = "04-playground-bucket${random_string.bucket_suffix.result}"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+
+  versioning {
+    enabled = true
+  }
+}
